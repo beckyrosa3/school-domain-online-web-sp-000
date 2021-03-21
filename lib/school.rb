b856f5bc20f155f3bead6b70eba30ddfd259e1a3
Becky Rosa <becky_m_rosa@yahoo.com>
2021-03-21 21:40:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+#require pry
+class School
+attr_accessor :name, :roster
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+    def add_student(name, grade)
+      @roster[grade] = []
+      @roster[grade] << name
+
+end
+end
+#binding.pry
